e5d2d536995144c2f9f21cbb63310e2b7a24ca87
mehsanrawat <0737@dawatulimaan.org>
2026-07-02 17:09:26 +0000

    added the getters and setters

--- index.js
+++ index.js
@@ -5,7 +5,32 @@ class Cuboid  {
         this.height = height
     }
 
+    get_length() { 
+        returnthis.length 
+    } 
+
+    set_length(new_length) { 
+        this.length = new_length
+    } 
+
+    get_width() { 
+        returnthis.width
+    } 
+
+    set_width(new_width) { 
+        this.width = new_width
+    } 
+
+    get_height() { 
+        returnthis.height
+    } 
+
+    set_height(new_height) { 
+        this.height = new_height
+    } 
 
 }
 
 const c = new Cuboid(5,3,4)
+
+
