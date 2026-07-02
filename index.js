95c94d93028c48cb6057a61d4c4aeb44e560f4c6
di-yahya <yahya.patel@dawatulimaan.org>
2026-07-02 16:06:21 +0000

    Make cuboid class

---
+++ index.js
@@ -0,0 +1,11 @@
+class Cuboid  {
+    constructor(length, width, height) {
+        this.length = length
+        this.width = width
+        this.height = height
+    }
+
+
+}
+
+const c = new Cuboid(5,3,4)
